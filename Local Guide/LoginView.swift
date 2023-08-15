02d1703cb24eadcec1088d9940926da04973ae6b
An Byungwook <abw2619@icloud.com>
2023-08-15 14:09:31 +0900

    LoginView

---
+++ Local Guide/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  Local Guide
+//
+//  Created by 안병욱의 mac on 2023/08/15.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
