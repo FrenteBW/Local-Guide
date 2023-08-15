dbe35d480a79c64e4d341922b7dbe6050320282e
An Byungwook <abw2619@icloud.com>
2023-08-15 14:00:54 +0900

    Initial Commit

---
+++ Local Guide/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Local Guide
+//
+//  Created by 안병욱의 mac on 2023/08/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Local Guide/Local_GuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  Local_GuideApp.swift
+//  Local Guide
+//
+//  Created by 안병욱의 mac on 2023/08/15.
+//
+
+import SwiftUI
+
+@main
+struct Local_GuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
